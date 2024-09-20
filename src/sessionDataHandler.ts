208ca7c7507d57144ca74502f1ac539926332fdb
Valentín Marchessi <valentin.marchessi@devsu.com>
2024-09-20 17:10:48 -0300

    Fix Endpoint

--- src/sessionDataHandler.ts
+++ src/sessionDataHandler.ts
@@ -107,7 +107,7 @@ export const sessionDataHandler = {
     let newTokenData;
 
     try {
-      newTokenData = await contactBackend<Token>("/initWebchat", { formData });
+      newTokenData = await contactBackend<Token>("/webchat/init", { formData });
     } catch (e) {
       throw Error("No results from server");
     }
