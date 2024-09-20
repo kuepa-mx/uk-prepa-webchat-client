23ae251104a05367c07ac3b5af188f7239c08f7a
Valentín Marchessi <valentin.marchessi@devsu.com>
2024-09-20 17:35:42 -0300

    Add Attributes to message sent

--- src/components/MessageInput.tsx
+++ src/components/MessageInput.tsx
@@ -14,156 +14,153 @@ import { FilePreview } from "./FilePreview";
 import { detachFiles } from "../store/actions/genericActions";
 import { CHAR_LIMIT } from "../constants";
 import {
-    formStyles,
-    innerInputStyles,
-    messageOptionContainerStyles,
-    filePreviewContainerStyles,
-    textAreaContainerStyles
+  formStyles,
+  innerInputStyles,
+  messageOptionContainerStyles,
+  filePreviewContainerStyles,
+  textAreaContainerStyles
 } from "./styles/MessageInput.styles";
 
 export const MessageInput = () => {
-    const dispatch = useDispatch();
-    const [text, setText] = useState("");
-    const [isSending, setIsSending] = useState(false);
-    const { conversation, attachedFiles, fileAttachmentConfig } = useSelector((state: AppState) => ({
-        conversation: state.chat.conversation,
-        attachedFiles: state.chat.attachedFiles || [],
-        fileAttachmentConfig: state.config.fileAttachment
-    }));
-    const oldAttachmentsLength = useRef((attachedFiles || []).length);
-    const textAreaRef = useRef<HTMLTextAreaElement>(null);
-    const attachmentsBoxRef = useRef<HTMLDivElement>(null);
+  const dispatch = useDispatch();
+  const [text, setText] = useState("");
+  const [isSending, setIsSending] = useState(false);
+  const { conversation, attachedFiles, fileAttachmentConfig, preEngagementData } = useSelector((state: AppState) => ({
+    conversation: state.chat.conversation,
+    attachedFiles: state.chat.attachedFiles || [],
+    fileAttachmentConfig: state.config.fileAttachment,
+    preEngagementData: state.session.preEngagementData
+  }));
+  const oldAttachmentsLength = useRef((attachedFiles || []).length);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const attachmentsBoxRef = useRef<HTMLDivElement>(null);
 
-    const throttleChange = useMemo(
-        () =>
-            throttle(() => {
-                conversation?.typing();
+  const throttleChange = useMemo(
+    () =>
+      throttle(() => {
+        conversation?.typing();
 
-                // in case the input was already focused, let's make sure to send the `read` status if the customer is typing
-                if (conversation?.lastReadMessageIndex !== conversation?.lastMessage?.index) {
-                    conversation?.setAllMessagesRead();
-                }
-            }, 500),
-        [conversation]
-    );
-
-    const isSubmitDisabled = (!text.trim() && !attachedFiles?.length) || isSending;
-
-    const send = async () => {
-        if (isSubmitDisabled) {
-            return;
+        // in case the input was already focused, let's make sure to send the `read` status if the customer is typing
+        if (conversation?.lastReadMessageIndex !== conversation?.lastMessage?.index) {
+          conversation?.setAllMessagesRead();
         }
-        if (!conversation) {
-            log.error("Failed sending message: no conversation found");
-            return;
-        }
-        setIsSending(true);
+      }, 500),
+    [conversation]
+  );
 
-        let preparedMessage = conversation.prepareMessage();
-        preparedMessage = preparedMessage.setBody(text);
-        attachedFiles.forEach((file: File) => {
-            const formData = new FormData();
-            formData.append(file.name, file);
-            preparedMessage.addMedia(formData);
-        });
-        await preparedMessage.build().send();
-        setText("");
-        dispatch(detachFiles(attachedFiles));
-        setIsSending(false);
-        textAreaRef.current?.focus();
-    };
+  const isSubmitDisabled = (!text.trim() && !attachedFiles?.length) || isSending;
 
-    const onKeyPress = (e: KeyboardEvent<HTMLTextAreaElement>) => {
-        if (e.key === "Enter" && !e.shiftKey) {
-            e.preventDefault();
-            (e.target as HTMLInputElement).form?.dispatchEvent(
-                new Event("submit", { cancelable: true, bubbles: true })
-            );
-        }
-    };
+  const send = async () => {
+    if (isSubmitDisabled) {
+      return;
+    }
+    if (!conversation) {
+      log.error("Failed sending message: no conversation found");
+      return;
+    }
+    if (!preEngagementData) {
+      log.error("Failed sending message: No data obtained in PreEngagement Phase");
+    }
+    setIsSending(true);
 
-    const onChange = (val: ChangeEvent<HTMLTextAreaElement>) => {
-        setText(val.target.value);
+    let preparedMessage = conversation.prepareMessage();
+    preparedMessage.setAttributes({ email: preEngagementData?.email, requestType: preEngagementData?.requestType });
+    preparedMessage = preparedMessage.setBody(text);
+    attachedFiles.forEach((file: File) => {
+      const formData = new FormData();
+      formData.append(file.name, file);
+      preparedMessage.addMedia(formData);
+    });
+    await preparedMessage.build().send();
+    setText("");
+    dispatch(detachFiles(attachedFiles));
+    setIsSending(false);
+    textAreaRef.current?.focus();
+  };
 
-        throttleChange();
-    };
+  const onKeyPress = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      (e.target as HTMLInputElement).form?.dispatchEvent(new Event("submit", { cancelable: true, bubbles: true }));
+    }
+  };
 
-    const onFocus = () => {
-        conversation?.setAllMessagesRead();
-    };
+  const onChange = (val: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(val.target.value);
 
-    useEffect(() => {
-        textAreaRef.current?.setAttribute("rows", "1");
-        textAreaRef.current?.focus();
-    }, [textAreaRef]);
+    throttleChange();
+  };
 
-    // Ensuring attached files are automatically scrolled to
-    useEffect(() => {
-        if (!attachmentsBoxRef.current) {
-            return;
-        }
+  const onFocus = () => {
+    conversation?.setAllMessagesRead();
+  };
 
-        if (attachedFiles.length > oldAttachmentsLength.current) {
-            (attachmentsBoxRef.current.lastChild as Element)?.scrollIntoView();
-        }
+  useEffect(() => {
+    textAreaRef.current?.setAttribute("rows", "1");
+    textAreaRef.current?.focus();
+  }, [textAreaRef]);
 
-        oldAttachmentsLength.current = attachedFiles.length;
-    }, [attachedFiles]);
+  // Ensuring attached files are automatically scrolled to
+  useEffect(() => {
+    if (!attachmentsBoxRef.current) {
+      return;
+    }
 
-    return (
-        <Box
-            as="form"
-            {...formStyles}
-            onSubmit={(e) => {
-                e.preventDefault();
-                send();
-            }}
-        >
-            <InputBox element="MESSAGE_INPUT_BOX" disabled={isSending}>
-                <Box as="div" {...innerInputStyles}>
-                    <Box {...textAreaContainerStyles}>
-                        <TextArea
-                            ref={textAreaRef}
-                            data-test="message-input-textarea"
-                            placeholder="Type your message"
-                            value={text}
-                            element="MESSAGE_INPUT"
-                            onChange={onChange}
-                            onFocus={onFocus}
-                            readOnly={isSending}
-                            onKeyPress={onKeyPress}
-                            maxLength={CHAR_LIMIT}
-                        />
-                    </Box>
-                    <Box {...messageOptionContainerStyles}>
-                        {fileAttachmentConfig?.enabled && <AttachFileButton textAreaRef={textAreaRef} />}
-                    </Box>
-                    <Box {...messageOptionContainerStyles}>
-                        <Button
-                            data-test="message-send-button"
-                            variant="primary_icon"
-                            size="icon_small"
-                            type="submit"
-                            aria-disabled={isSubmitDisabled}
-                        >
-                            <SendIcon decorative={false} title="Send message" size="sizeIcon30" />
-                        </Button>
-                    </Box>
-                </Box>
-                {attachedFiles && (
-                    <Box data-test="message-attachments" {...filePreviewContainerStyles} ref={attachmentsBoxRef}>
-                        {attachedFiles.map((file, index) => (
-                            <FilePreview
-                                focusable={true}
-                                key={index}
-                                file={file}
-                                isBubble={false}
-                                disabled={isSending}
-                            />
-                        ))}
-                    </Box>
-                )}
-            </InputBox>
+    if (attachedFiles.length > oldAttachmentsLength.current) {
+      (attachmentsBoxRef.current.lastChild as Element)?.scrollIntoView();
+    }
+
+    oldAttachmentsLength.current = attachedFiles.length;
+  }, [attachedFiles]);
+
+  return (
+    <Box
+      as="form"
+      {...formStyles}
+      onSubmit={(e) => {
+        e.preventDefault();
+        send();
+      }}
+    >
+      <InputBox element="MESSAGE_INPUT_BOX" disabled={isSending}>
+        <Box as="div" {...innerInputStyles}>
+          <Box {...textAreaContainerStyles}>
+            <TextArea
+              ref={textAreaRef}
+              data-test="message-input-textarea"
+              placeholder="Type your message"
+              value={text}
+              element="MESSAGE_INPUT"
+              onChange={onChange}
+              onFocus={onFocus}
+              readOnly={isSending}
+              onKeyPress={onKeyPress}
+              maxLength={CHAR_LIMIT}
+            />
+          </Box>
+          <Box {...messageOptionContainerStyles}>
+            {fileAttachmentConfig?.enabled && <AttachFileButton textAreaRef={textAreaRef} />}
+          </Box>
+          <Box {...messageOptionContainerStyles}>
+            <Button
+              data-test="message-send-button"
+              variant="primary_icon"
+              size="icon_small"
+              type="submit"
+              aria-disabled={isSubmitDisabled}
+            >
+              <SendIcon decorative={false} title="Send message" size="sizeIcon30" />
+            </Button>
+          </Box>
         </Box>
-    );
+        {attachedFiles && (
+          <Box data-test="message-attachments" {...filePreviewContainerStyles} ref={attachmentsBoxRef}>
+            {attachedFiles.map((file, index) => (
+              <FilePreview focusable={true} key={index} file={file} isBubble={false} disabled={isSending} />
+            ))}
+          </Box>
+        )}
+      </InputBox>
+    </Box>
+  );
 };
--- src/components/MessagingCanvasPhase.tsx
+++ src/components/MessagingCanvasPhase.tsx
@@ -12,21 +12,21 @@ import { notifications } from "../notifications";
 import { AttachFileDropArea } from "./AttachFileDropArea";
 
 export const MessagingCanvasPhase = () => {
-    const dispatch = useDispatch();
-    const conversationState = useSelector((state: AppState) => state.chat.conversationState);
+  const dispatch = useDispatch();
+  const conversationState = useSelector((state: AppState) => state.chat.conversationState);
 
-    useEffect(() => {
-        dispatch(removeNotification(notifications.failedToInitSessionNotification("ds").id));
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(removeNotification(notifications.failedToInitSessionNotification("ds").id));
+  }, [dispatch]);
 
-    const Wrapper = conversationState === "active" ? AttachFileDropArea : Fragment;
+  const Wrapper = conversationState === "active" ? AttachFileDropArea : Fragment;
 
-    return (
-        <Wrapper>
-            <Header />
-            <NotificationBar />
-            <MessageList />
-            {conversationState === "active" ? <MessageInput /> : <ConversationEnded />}
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <Header />
+      <NotificationBar />
+      <MessageList />
+      {conversationState === "active" ? <MessageInput /> : <ConversationEnded />}
+    </Wrapper>
+  );
 };
